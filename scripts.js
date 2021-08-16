1672db3485c17e96069bd9ff0025a11bda7dfcab
Anton <68000966+Kri0k@users.noreply.github.com>
2021-08-16 23:05:29 +0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-var images = [
-	'./resources/images/hotel.png',
-	'./resources/images/lake.png',
-	'./resources/images/statue.png',
-	'./resources/images/window.png'
-]
-var randimg =Math.floor(Math.random()*4);
-document.body.background = images[randimg];
